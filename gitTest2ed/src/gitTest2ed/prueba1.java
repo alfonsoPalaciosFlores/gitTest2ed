630fcfa25946bff50e9a4305111c3766e4fc72e3
alfonsoPalaciosFlores <65362681+alfonsoPalaciosFlores@users.noreply.github.com>
2020-05-21 14:09:45 +0200

    Update prueba1.java

--- gitTest2ed/src/gitTest2ed/prueba1.java
+++ gitTest2ed/src/gitTest2ed/prueba1.java
@@ -3,7 +3,8 @@ package gitTest2ed;
 public class prueba1 {
 int edad=36;
 String nombre="Alfonso Palacios Flores";
-String direccion="calle pinito del oro nº 4, Almeria";
+String direccion="calle pinito del oro nÂº 4, Almeria";
 String profesion="estudiante";
-String curso="1ºDAM";
+String curso="1ÂºDAM";
+String asignatura="entornos de desarrollo";
 }
