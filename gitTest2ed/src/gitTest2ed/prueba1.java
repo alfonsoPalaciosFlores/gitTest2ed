dcacd90ddaa67987383bb38e7c4b5a36f7a23a4a
alfonso palacios <alfonso palacios@alfonso.home>
2020-05-21 13:01:43 +0200

    commit nº3 (añado mas variables inicializadas)

--- gitTest2ed/src/gitTest2ed/prueba1.java
+++ gitTest2ed/src/gitTest2ed/prueba1.java
@@ -4,4 +4,6 @@ public class prueba1 {
 int edad=36;
 String nombre="Alfonso Palacios Flores";
 String direccion="calle pinito del oro nº 4, Almeria";
+String profesion="estudiante";
+String curso="1ºDAM";
 }
